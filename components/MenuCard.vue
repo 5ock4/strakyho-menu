3b5b49f01cd1c295707ef46d7b4b07047e663f5b
Adam Strakoš <strakosadam@gmail.com>
2021-04-04 18:05:00 +0200

    initial commit

---
+++ components/MenuCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <p class="display-1 text--primary">
+        Kuře s ryžu
+      </p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal accent-4"
+      >
+        Learn More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      reveal: false,
+    }),
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <menu-card />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import MenuCard from '~/components/MenuCard.vue'
+
+export default {
+  components: {
+    MenuCard
+  },
+}
+</script>
