923cb189c80ce349ccb4d858277a6014e9b58250
Adam Strakoš <strakosadam@gmail.com>
2021-04-11 14:39:16 +0200

    add new files

---
+++ store/menu.js
@@ -0,0 +1,20 @@
+// export const state = () => ({
+//   curr_menu: [],
+//   previous_menu: [],
+//   history_menu: []
+// })
+
+// export const mutations = {
+//   init(state) {
+//     state.curr_menu = 
+//   }
+//   add(state, menu_elem) {
+//     state.curr_menu.push(menu_elem)
+//   },
+//   remove(state, { todo }) {
+//     state.list.splice(state.list.indexOf(todo), 1)
+//   },
+//   toggle(state, todo) {
+//     todo.done = !todo.done
+//   }
+// }
