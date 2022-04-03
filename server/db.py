fdf51009dcdfeb9e8dd2e11e69b554f1412ff8aa
5ock4 <strakosadam@gmail.com>
2022-04-03 15:40:18 +0200

    test

--- server/db.py
+++ server/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 # Global Variables
-SQLITE                  = 'sqlite'
+SQLITE = 'sqlite'
 
 # Table Names
 USERS           = 'users'
